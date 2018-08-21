911631fbb2c136b4e276da62ac6a0229d801ca2e
m26415172 <m26415172@john.petra.ac.id>
2018-08-21 01:47:07 -0700

    fix bug login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,9 @@ class HomeController < ApplicationController
 
   def login
     if params[:username].present? && params[:password].present?
-      loggedin = User.login(params[:username], params[:password])
-      loggedin ? render :index : redirect_to :back
+      @loggedin = User.login(params[:username], params[:password])
+      flash[:notice] = 'Loggedin' if @loggedin
+      @loggedin ? (render :index) : (redirect_back fallback_location: root_path)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
+LOGGED_IN_USER_KEY = "loggedInUser".freeze
+
 class User < ApplicationRecord
 	has_many :candidates
 	has_many :voters
 
-	def login(username, password)
+	def self.login(username, password)
 		user = User.find_by(:username => username, :password => password)
+		$redis.set(LOGGED_IN_USER_KEY+user.id.to_s, Time.now) if user.present?
 		user.present?
 	end
 end
