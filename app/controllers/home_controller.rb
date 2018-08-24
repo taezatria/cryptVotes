6b8ce2e1d77a2e34d072064775736c34337e5ad0
m26415172 <m26415172@john.petra.ac.id>
2018-08-23 22:07:05 -0700

    fixing bug and login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
 
   def login
     @params = params
-    render :index
+    if params[:username].present? && params[:password].present?
+      user = User.login(params[:username], params[:password])
+      @params[:user] = user
+      user ? (render :dashboard) : (redirect_back fallback_location: root_path)
+    end
   end
 end
