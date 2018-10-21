5c77665c93786aa5f7659e869d240eaf1113d081
m26415172 <m26415712@john.petra.ac.id>
2018-10-21 20:31:41 +0700

    email view pages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,29 @@ class UserMailer < ApplicationMailer
     @url = "http://192.168.108.134:6789/home/"+@user.id.to_s+"/setup"
     mail(to: @user.email, subject: 'E-mail Verification')
   end
+
+  def anounce_election
+    @user = params[:user]
+    @election = params[:election]
+    mail(to: @user.email, subject: 'Election Date Reminder')
+  end
+
+  def forget_password
+    @user = params[:user]
+    @url = ''
+    mail(to: @user.email, subject: 'Forget Password Verification')
+  end
+
+  def passphrase_reset
+    @user = params[:user]
+    @url = ''
+    mail(to: @user.email, subject: 'Passphrase Reset Request')
+  end
+
+  def block_mined
+    @user = params[:user]
+    @blockhash = params[:blockhash]
+    @txid = params[:txid]
+    mail(to: @user.email, subject: 'Ballot Block Detail')
+  end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -7,4 +7,20 @@ class UserMailerPreview < ActionMailer::Preview
   def verification_email
     UserMailer.with(user: User.first).verification_email
   end
+
+  def anounce_election
+    UserMailer.with(user: User.first, election: Election.first).anounce_election
+  end
+
+  def forget_password
+    UserMailer.with(user: User.first).forget_password
+  end
+
+  def passphrase_reset
+    UserMailer.with(user: User.first).passphrase_reset
+  end
+
+  def block_mined
+    UserMailer.with(user: User.first).block_mined
+  end
 end
