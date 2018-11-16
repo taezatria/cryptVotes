91cab4e236815067b7029a064ce9b160922155cd
m26415172 <m26415172@john.petra.ac.id>
2018-11-15 22:58:27 -0800

    add folder keysdata and gitignore

--- lib/opssl.rb
+++ lib/opssl.rb
@@ -4,41 +4,41 @@ module OpSSL
     end
 
     def genpkey(user, pass)
-      IO.popen("openssl genpkey -algorithm RSA -out ~/.keysdat/"+user.to_s+"_pkey.pem -aes-256-cbc -pass pass:"+pass.to_s) do |f|
+      IO.popen("openssl genpkey -algorithm RSA -out .keysdata/"+user.to_s+"_pkey.pem -aes-256-cbc -pass pass:"+pass.to_s) do |f|
         f.close
         $?.to_i == 0
       end
     end
 
     def genpbkey(user, pass)
-      IO.popen("openssl rsa -pubout -in ~/.keysdat/"+user.to_s+"_pkey.pem -passin pass:"+pass.to_s+" -out ~/.keysdat/"+user.to_s+"_pbkey.pem") do |f|
+      IO.popen("openssl rsa -pubout -in .keysdata/"+user.to_s+"_pkey.pem -passin pass:"+pass.to_s+" -out .keysdata/"+user.to_s+"_pbkey.pem") do |f|
         f.close
         $?.to_i == 0
       end
     end
 
     def encrypt(user, data)
-      IO.popen("echo '"+data.to_s+"' | openssl rsautl -encrypt -inkey ~/.keysdat/"+user.to_s+"_pbkey.pem -pubin | xxd -p -c 999") do |f|
+      IO.popen("echo '"+data.to_s+"' | openssl rsautl -encrypt -inkey .keysdata/"+user.to_s+"_pbkey.pem -pubin | xxd -p -c 999") do |f|
         f.gets
       end
     end
 
     def decrypt(user, pass, data)
-      IO.popen("echo '"+data.to_s+"' | xxd -p -r | openssl rsautl -decrypt -inkey ~/.keysdat/"+user.to_s+"_pkey.pem -passin pass:"+pass.to_s) do |f|
+      IO.popen("echo '"+data.to_s+"' | xxd -p -r | openssl rsautl -decrypt -inkey .keysdata/"+user.to_s+"_pkey.pem -passin pass:"+pass.to_s) do |f|
         dat = f.gets 
         dat.chomp if dat.present?
       end
     end
 
     def del_pub(user)
-      IO.popen("rm ~/.keysdat/"+user.to_s+"_pbkey.pem") do |f|
+      IO.popen("rm .keysdata/"+user.to_s+"_pbkey.pem") do |f|
         f.close
         $?.to_i == 0
       end
     end
 
     def del_pkey(user)
-      IO.popen("rm ~/.keysdat/"+user.to_s+"_pkey.pem") do |f|
+      IO.popen("rm .keysdata/"+user.to_s+"_pkey.pem") do |f|
         f.close
         $?.to_i == 0
       end
