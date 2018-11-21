c95876f1edfc56a789f0e2c5689fd501abec5752
m26415172 <m26415172@john.petra.ac.id>
2018-11-20 22:29:24 -0800

    add jsencrypt

--- script/check_election.rb
+++ script/check_election.rb
@@ -5,12 +5,12 @@ Election.where(deleted_at: nil).each do |election|
 			election.save
 		end
 	elsif election.status == 1
-		if DateTime.now == election.end_date
+		if DateTime.now == election.end_date + 1.days
 			election.status = 2
 			election.save
 		end
 	elsif election.status == 2
-		if DateTime.now + 6.days == election.end_date + 6.days
+		if DateTime.now == election.end_date + 6.days
 			election.status = 3
 			election.save
 		end
